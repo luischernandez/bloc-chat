221825b6290f3841af56cd69fd601d3aa44c03d2
luischernandez <luishernandez01@live.com>
2017-09-13 20:15:21 -0700

    Make rooms

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,10 +7,10 @@
             });
 
         $stateProvider
-            .state('home', {
+            .state('Room', {
                 url: '/',
-                controller: 'HomeCtrl as home',
-                templateUrl: '/templates/home.html'
+                controller: 'RoomCtrl as Room',
+                templateUrl: '/templates/Room.html'
             });
     }
 
