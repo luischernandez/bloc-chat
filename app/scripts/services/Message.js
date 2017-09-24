62e426cd3c90778225171505d364c925b1fa8224
luischernandez <luishernandez01@live.com>
2017-09-24 00:15:35 -0700

    list messages incomplete 2

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,14 +1,19 @@
 (function() {
   function Message($firebaseArray) {
     var Message = {};
-    var ref = firebase.database().ref().child("messages");
-    Message.messages = $firebaseArray(ref);
+    // var ref = firebase.database().ref().child("messages");
+    // Message.messages = $firebaseArray(ref);
 
     Message.getByRoomId = function(roomId) {
         // Filter the messages by their room ID.
         //return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
-        var ref = new Firebase("https://bloc-chat-5b77e.firebaseio.com/messages");
-        return ref.orderByChild('roomId').equalTo(roomId);
+        // var ref = new Firebase("https://bloc-chat-5b77e.firebaseio.com/messages");
+        // return ref.orderByChild('roomId').equalTo(roomId);
+        var ref = firebase.database().ref().child("messages");
+        var messages = $firebaseArray(ref);
+        console.log(messages);
+        return messages;
+
     };
 
     return Message;
